cba4781db239a5745d7d46aa3a809804e1fc678b
Matthew Petersen <vms@pobox.com>
2021-07-05 15:13:13 +1000

    added pushbullet and simplified settings

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Monitors Docker (MONitors dOCKER) containers and alerts on state change
 ![Telegram Alerts](https://raw.githubusercontent.com/petersem/monocker/master/doco/telegram.PNG)
 
 ## Features
-- Telegram messaging integration
+- Telegram integration
+- Pushbullet integration
 - Monitors all containers (every 10 seconds)
 
 ## Future Considerations
@@ -21,13 +22,17 @@ services:
     container_name: monocker
     image: petersem/monocker
     environment:
-      NODE_ENV: production
-      TELEGRAM_ID: 'your_telegram_id'
-      TELEGRAM_CHAT_ID: 'your_telegram_chat_id'
+      # Specify the messaging platform and details, or leave blank if only wanting container logs (pick one only)
+      MESSAGE_PLATFORM: 'telegram@your_bot_id@your_chat_id'
+      # MESSAGE_PLATFORM: 'pushbullet@your_api_key@your_device_id'
+      # MESSAGE_PLATFORM: 'pushbullet@your_api_key@your_device_id'
+      # MESSAGE_PLATFORM: ''
     volumes:
       - /var/run/docker.sock:/var/run/docker.sock
-    ports:
-      - 3300:3000
     restart: unless-stopped
 ```
-> Please see Telegram documentation for how to obtain ID values. 
+> For Telegram: See documentation for how to obtain ID values. 
+
+> For Pushbullet: Open Pushbullet in a browser and get device ID from URL [Example](https://raw.githubusercontent.com/petersem/monocker/master/doco/telegram.PNG)
+
+This application uses *semantic* versioning. See [here](https://semver.org/) for more details. 
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Telegram = require('telegram-notify');
 const Docker = require('dockerode');
 const pjson = require("./package.json");
+const PushBullet = require('pushbullet');
 
 let docker = new Docker({socketPath: '/var/run/docker.sock'});
 // var docker = new Docker({
@@ -8,12 +9,12 @@ let docker = new Docker({socketPath: '/var/run/docker.sock'});
 //     host: 'localhost',
 //     port: 2375 //process.env.DOCKER_PORT || 2375
 // });
+const NODE_ENV = process.env.NODE_ENV || "production";
+const MESSAGE_PLATFORM = process.env.MESSAGE_PLATFORM || "";
+const LABEL_ENABLE = process.env.MONOCKER_LABEL_DISABLE || 'false';
+const LABEL_DISABLE = process.env.MONOCKER_LABEL_DISABLE || 'false';
 
-const TID = process.env.TELEGRAM_ID; // || "1472584417:AAFW1zbGk17i8kGeTLAByLxmwkaBScWhqvo";
-const TCID = process.env.TELEGRAM_CHAT_ID; // || "-448418280";
-const LABEL_ENABLE = process.env.MONOCKER_LABEL_DISABLE || 'false'
-const LABEL_DISABLE = process.env.MONOCKER_LABEL_DISABLE || 'false'
-
+let msgDetails = MESSAGE_PLATFORM.split('@');
 
 let monContainers = [];
 console.log("-------------------------------------------------------");
@@ -23,14 +24,43 @@ console.log(" ");
 console.log(" Version: " + pjson.version);
 console.log("-------------------------------------------------------");
 
-async function send(message){
-    let notify = new Telegram({token:TID, chatId:TCID});
+async function sendTelegram(message){
+    let notify = new Telegram({token:msgDetails[1], chatId:msgDetails[2]});
     await notify.send(message, {timeout: 10000}, {parse_mode: 'html'});
 }
 
+async function sendPushbullet(title, message){
+    var pusher = new PushBullet(msgDetails[1]);
+
+    pusher.note(msgDetails[2], title, message, function(error, response) {});
+}
+
+async function sendPushover(title, body){
+    //let notify = new Telegram({token:TID, chatId:TCID});
+    //await notify.send(message, {timeout: 10000}, {parse_mode: 'html'});
+}
+
+
+async function send(message) {
+    switch(msgDetails[0].toLowerCase()) {
+        case "telegram":
+            sendTelegram(`<b>MONOCKER</b>
+` + message);
+            break;
+        case "pushbullet":
+            sendPushbullet("MONOCKER", message);
+            break;
+        case "pushover":
+            // not implemented
+            break;
+        case "default":
+            console.log("* No valid messing platform specified - logged only")
+    }
+}
+
 async function list(){
-    let now = new Date();
-    console.log(now.toLocaleString() + " - Container scan");
+    //let now = new Date();
+    //console.log(now.toLocaleString() + " - Container scan");
     docker.listContainers({all: true}, function(err, containers) {
         // check for changes in status (first run is populating data only)
         let newConArray = [];
@@ -41,8 +71,7 @@ async function list(){
             if(c.Status.includes("(unhealthy)")) hcStatus="(unhealthy)"
             if(monContainers.includes(c.Id + "," + c.State + "," + hcStatus) == false && monContainers.length !== 0 ){
                 console.log("    - " +c.Names[0].replace("/","") + ": " + c.State + " " + hcStatus);
-                send(`<b>MONOCKER</b>
-` + c.Names[0].replace("/","") +": <i>"+c.State + " " + hcStatus + "</i>")
+                send(c.Names[0].replace("/","") +": "+c.State + " " + hcStatus)
             }
             // create new container array
             newConArray.push(c.Id + "," + c.State + "," + hcStatus);
@@ -70,6 +99,6 @@ async function run(){
     await list();
 }
 
-send(`<b>MONOCKER</b>
-Monitoring started`)
+send("Monitoring started")
+console.log("Monitoring started")
 setInterval(run,10000);
